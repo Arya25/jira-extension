1ed87edad6c4376367ca22ef2766b48b577cc380
Rahul Gupta <rgupta2@atlassian.com>
2021-10-24 03:07:11 +0530

    Refinements
    Summary:
    - Remove permissions which are not required
    - Add Shuffle icon in supported dimensions
    - Error handling with try catch

--- main.js
+++ main.js
@@ -1,107 +1,112 @@
-// Refers to the existing action buttons to create a shuffle button
-function getShuffleButton(referenceLi) {
-  const li = referenceLi.cloneNode(true);
-  var button = li.querySelector("button");
-  button.querySelector('span').innerHTML = "Shuffle";
+try {
+  console.log('script running');
+  // Refers to the existing action buttons to create a shuffle button
+  function getShuffleButton(referenceLi) {
+    const li = referenceLi.cloneNode(true);
+    var button = li.querySelector("button");
+    button.querySelector('span').innerHTML = "Shuffle";
 
-  return li;
-}
-
-// Append the shuffle button within the existing actions on the board
-// like 'Recently Updated', 'My issues'
-function appendShuffleButton() {
-  const actions = document.querySelector('.ghx-backlog-search-container').parentNode.lastChild;
-  const actionsList = actions.querySelector('ul');
-  const shuffleLi = getShuffleButton(actionsList.querySelector('li'));
-  actionsList.appendChild(shuffleLi);
-
-  return shuffleLi.querySelector('button');
-}
-
-// Since the board is client rendered it can take a while even after
-// DOMContentLoaded event for the board dom elements to be available to query
-// The function polls for when the board is ready. Cancels execution if not ready in 7 seconds
-const isBoardReady = (cb) => {
-  const isReady = () => !!document.querySelector('.ghx-backlog-search-container');
-  if (isReady()) {
-    cb();
-    return;
+    return li;
   }
 
-  let count = 0;
-  let poll;
-  poll = setInterval(() => {
-    // Stop after 7 tries
-    if (count === 7) {
-      clearInterval(poll);
-      return;
-    }
+  // Append the shuffle button within the existing actions on the board
+  // like 'Recently Updated', 'My issues'
+  function appendShuffleButton() {
+    const actions = document.querySelector('.ghx-backlog-search-container').parentNode.lastChild;
+    const actionsList = actions.querySelector('ul');
+    const shuffleLi = getShuffleButton(actionsList.querySelector('li'));
+    actionsList.appendChild(shuffleLi);
 
+    return shuffleLi.querySelector('button');
+  }
+
+  // Since the board is client rendered it can take a while even after
+  // DOMContentLoaded event for the board dom elements to be available to query
+  // The function polls for when the board is ready. Cancels execution if not ready in 7 seconds
+  const isBoardReady = (cb) => {
+    const isReady = () => !!document.querySelector('.ghx-backlog-search-container');
     if (isReady()) {
       cb();
-      clearInterval(poll);
       return;
     }
 
-    count++;
-  }, 1000)
-};
+    let count = 0;
+    let poll;
+    poll = setInterval(() => {
+      // Stop after 7 tries
+      if (count === 7) {
+        clearInterval(poll);
+        return;
+      }
 
-isBoardReady(() => {
-  const shuffleButton = appendShuffleButton();
-  shuffleButton.addEventListener("click", () => {
-    function shuffleJiraMembers() {
-      const printMember = el => el.outerText.split(' ')[0];
-      const printMembers = (arr) => arr.map(printMember);
-      const getMembers = parent => Array.from(parent.querySelectorAll('.ghx-swimlane'));
-      const getMemberHeader = member => member.querySelector('.ghx-swimlane-header');
+      if (isReady()) {
+        clearInterval(poll);
+        cb();
+        return;
+      }
 
-      // https://bost.ocks.org/mike/shuffle/
-      const shuffle = (members) => {
-        let m = members.length - 1;
-        let parent;
+      count++;
+    }, 1000)
+  };
 
-        // While there remain elements to shuffle…
-        while (m > 0) {
-          // Pick a remaining element…
-          const i = Math.floor(Math.random() * m);
-          console.log(i, m, 'lalaji')
+  isBoardReady(() => {
+    const shuffleButton = appendShuffleButton();
+    shuffleButton.addEventListener("click", () => {
+      function shuffleJiraMembers() {
+        const printMember = el => el.outerText.split(' ')[0];
+        const printMembers = (arr) => arr.map(printMember);
+        const getMembers = parent => Array.from(parent.querySelectorAll('.ghx-swimlane'));
+        const getMemberHeader = member => member.querySelector('.ghx-swimlane-header');
 
-          // And swap it with the current element.
-          const temp = document.createElement('temp');
+        // https://bost.ocks.org/mike/shuffle/
+        const shuffle = (members) => {
+          let m = members.length - 1;
+          let parent;
 
-          parent = members[m].parentNode;
-          console.log(printMembers(Array.from(parent.querySelectorAll('.ghx-swimlane'))));
-          console.log(`picked ${printMember(members[i])}, replaced with: ${printMember(members[m])}`);
-          // a -> temp
-          parent.replaceChild(temp, members[m]);
-          // b -> a
-          parent.replaceChild(members[m], members[i]);
-          // temp -> b
-          parent.replaceChild(members[i], temp);
-          m--;
-        }
+          // While there remain elements to shuffle…
+          while (m > 0) {
+            // Pick a remaining element…
+            const i = Math.floor(Math.random() * m);
+            console.log(i, m, 'lalaji')
 
-        return getMembers(parent);
-      };
+            // And swap it with the current element.
+            const temp = document.createElement('temp');
 
-      const firstMember = (className = 'ghx-first') => ({
-        unmark: (member) => member.classList.remove(className),
-        mark: (member) => member.classList.add(className),
-      });
-      const { mark, unmark } = firstMember();
+            parent = members[m].parentNode;
+            console.log(printMembers(Array.from(parent.querySelectorAll('.ghx-swimlane'))));
+            console.log(`picked ${printMember(members[i])}, replaced with: ${printMember(members[m])}`);
+            // a -> temp
+            parent.replaceChild(temp, members[m]);
+            // b -> a
+            parent.replaceChild(members[m], members[i]);
+            // temp -> b
+            parent.replaceChild(members[i], temp);
+            m--;
+          }
 
-      const pool = document.querySelector('#ghx-pool');
-      const members = getMembers(pool);
-      unmark(members[0]);
+          return getMembers(parent);
+        };
 
-      const shuffledMembers = shuffle(members);
-      const firstShuffledMember = shuffledMembers[0];
+        const firstMember = (className = 'ghx-first') => ({
+          unmark: (member) => member.classList.remove(className),
+          mark: (member) => member.classList.add(className),
+        });
+        const { mark, unmark } = firstMember();
 
-      const headerWrapper = pool.querySelector('#ghx-swimlane-header-stalker');
-      headerWrapper.replaceChild(getMemberHeader(firstShuffledMember).cloneNode(true), getMemberHeader(headerWrapper));
-      mark(firstShuffledMember);
-    }
-    shuffleJiraMembers();
+        const pool = document.querySelector('#ghx-pool');
+        const members = getMembers(pool);
+        unmark(members[0]);
+
+        const shuffledMembers = shuffle(members);
+        const firstShuffledMember = shuffledMembers[0];
+
+        const headerWrapper = pool.querySelector('#ghx-swimlane-header-stalker');
+        headerWrapper.replaceChild(getMemberHeader(firstShuffledMember).cloneNode(true), getMemberHeader(headerWrapper));
+        mark(firstShuffledMember);
+      }
+      shuffleJiraMembers();
+    });
   });
-});
+} catch (error) {
+  console.log('Error', error);
+}
